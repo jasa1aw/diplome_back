0a08d6cb6ed442f85a2ee36aac4be1e07be74b3b
jasa1aw <erakrut05@gmail.com>
2024-06-14 22:32:41 +0500

    edit routes

--- app/products/routes.js
+++ app/products/routes.js
@@ -8,7 +8,7 @@ const {isAdmin} = require('../auth/middlewares')
 router.post('/api/product/addProduct', passport.authenticate('jwt', { session: false }), isAdmin, upload.single('image'), addProduct);
 router.put('/api/product/editProduct', passport.authenticate('jwt', { session: false }), isAdmin, upload.single('image'), editProduct)
 router.delete('/api/product/deleteProduct/:id', passport.authenticate('jwt', { session: false }), isAdmin, deleteProductByID);
-router.get('/api/product/getAllProducts', passport.authenticate('jwt', { session: false }), getAllProducts);
+router.get('/api/product/getAllProducts', getAllProducts);
 router.get('/api/product/getProductByID/:id', passport.authenticate('jwt', { session: false }), getProductByID);
 
 module.exports = router;
